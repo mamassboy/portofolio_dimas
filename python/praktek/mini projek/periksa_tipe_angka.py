64a7f14337f66c2d1e45026144f6c9f0e1e5184f
Dimas Arya Arjuna <dimasaryarjuna06@gmail.com>
2025-09-13 09:47:21 +0700

    Menambahkan projek memeriksa tipe bilangan dari sebuah angka

---
+++ python/praktek/mini projek/periksa_tipe_angka.py
@@ -0,0 +1,22 @@
+angka = int(input("Masukkan angka yang ingin Anda periksa: "))
+
+
+# Cek Positif, Negatif, atau Netral
+if angka > 0:
+    sifat = "Positif"
+elif angka < 0:
+    sifat = "Negatif"
+else:
+    sifat = "Netral"
+
+if angka == 0 :
+    print(f'{angka} Adalah Bilangan {sifat}')
+else: 
+    if angka % 2 == 0:
+        jenis = "Genap"
+    else:
+        jenis = " Ganjil"
+    print(f'{angka} Adalah Bilangan {sifat}')
+    print("dan")
+    print(f'{angka} Adalah Bilangan {jenis}')
+        
