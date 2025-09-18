8daa0d2ce7ac5a0010fdddc88d1d2b18e10ee78d
Dimas Arya Arjuna <dimasaryarjuna06@gmail.com>
2025-09-18 10:57:49 +0700

    Memperbaiki kesalahan pada rumus luas dan penggunaan library math

--- Latihan Asdos/Pbo/lingkaran.py
+++ Latihan Asdos/Pbo/lingkaran.py
@@ -1,12 +1,12 @@
-
+import math
 class Lingkaran:
     def __init__(self, r):
         self.r = r
 
     def luas(self):
-        return 3.14 * self.r
+        return math.pi * (self.r**2)
     def keliling(self):
-        return 2 * 3.14 * self.r
+        return 2 * math.pi * self.r
     
 jari_jari = int(input("Masukkan jari Jari Lingkaran Anda: "))
 l = Lingkaran(jari_jari)
